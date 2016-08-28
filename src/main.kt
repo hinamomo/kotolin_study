0a1cff0406a89b8d6f480040e8eb659dd147be69
Momoko Ishimura <applemelody03@gmail.com>
2016-08-28 16:23:29 +0900

    first kotolin commit

---
+++ src/main.kt
@@ -0,0 +1,11 @@
+/**
+ * Created by momoko on 2016/08/21.
+ */
+package sample
+
+fun main(args: Array<String>){
+    val userName: String ="たろう"
+    val userAge: Int = 28
+    println("${userName}さんは${userAge}歳です")
+
+}
